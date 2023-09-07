681caf2437ee628d341cd58cdeadae694b025778
John R <0001reich@gmail.com>
2023-09-07 14:24:33 -0400

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -6,6 +6,23 @@
 
 // ╚═════════════════════════════════════════════════════════════════════════╝
 
+function arraypush() 
+    {
+        const chores = ['wash dishes', 'do laundry', 'take out trash'];
+        console.log(chores);
+        chores.push('heh', 'hah');
+        console.log(chores);
+    }
+
+function popXmpl() 
+    {
+        const chores2 = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+        chores2.pop();
+        console.log(chores2);
+    }
+
+
+
 
 
 
