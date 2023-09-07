d3cd58d38d09053109920610048260492ded1642
John R <0001reich@gmail.com>
2023-09-07 15:34:18 -0400

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -17,9 +17,48 @@ function arraypush()
 function popXmpl() 
     {
         const chores2 = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+        console.log(chores2);
         chores2.pop();
         console.log(chores2);
     }
+
+function arraymethodPlus()
+    {
+        var whitespace = '              '
+
+        const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+    
+        console.log('╔══════════════════════════════════╤ Unmodified ╤═══════════════════════════════════════╗');
+            console.log();
+            console.log(whitespace + groceryList);
+            console.log();
+    
+        console.log('╠═════════════════════════════════════╤ Shift method ╤══════════════════════════════════╣');
+            groceryList.shift();
+            console.log();
+            console.log(whitespace + groceryList);
+            console.log();
+    
+        console.log('╠═════════════════════════════════════╤ unShift method ╤════════════════════════════════╣');
+            groceryList.unshift('popcorn');
+    
+            console.log();
+            console.log(whitespace + groceryList);
+            console.log();
+    
+        console.log('╠═══════════════════════════════════╤ .slice method ╤═══════════════════════════════════╣');
+    
+            console.log(whitespace + whitespace + groceryList.slice(1, 4));
+            //  console.log(groceryList);
+      
+        console.log('╠══════════════════════════════╤ .index method ╤════════════════════════════════════════╣');
+            const pastaIndex = groceryList.indexOf('pasta');
+            console.log();
+            console.log(whitespace + whitespace + whitespace + pastaIndex);
+            console.log();
+        console.log('╚═══════════════════════════════════════════════════════════════════════════════════════╝');
+    }
+
 
 
 
