1775663aa5169e1f608e53f4518329b4171cf8b6
John R <0001reich@gmail.com>
2023-09-18 16:03:34 -0400

    added more samples

--- scripts/test.js
+++ scripts/test.js
@@ -22,7 +22,8 @@ function popXmpl()
         console.log(chores2);
     }
 
-function arraymethodPlus() {
+function arraymethodPlus() 
+    {
         var whitespace = '              '
         const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
     
@@ -55,17 +56,61 @@ function arraymethodPlus() {
             console.log(whitespace + whitespace + whitespace + pastaIndex);
             console.log();
         console.log('╚═══════════════════════════════════════════════════════════════════════════════════════╝');
-}
+    }
 
 
 
-function forloop() {
-    for (let count = 0 + 5; count < 11; count++) {
-        console.log(count);
-      }
-}
+function forloop() 
+    {
+        for (let count = 0 + 5; count < 11; count++) { console.log(count); }
+    }
 
-function forLoopCountdown() {
-    for (let counter = 3; counter >= 0; counter--) 
-    { console.log(counter); }
-}
+function forLoopCountdown() 
+    {
+        for (let counter = 3; counter >= 0; counter--) { console.log(counter); }
+    }
+
+function nestedloop() 
+    {
+        let bobsFollowers = [ 'you', 'have', 'no', 'friends' ];
+        let tinasFollowers = [ 'sorry', 'no', 'friends' ];
+        let mutualFollowers = [];
+
+        for (let i = 0; i < bobsFollowers.length; i++) 
+            {
+                for (let j = 0; j < tinasFollowers.length; j++) { 
+                    if (bobsFollowers[i] === tinasFollowers[j]) { mutualFollowers.push(bobsFollowers[i]); }
+                    }
+            };
+    }
+
+function whileloop() 
+    {
+        const cards = ['diamond', 'spade', 'heart', 'club'];
+        let currentCard;
+
+        while (currentCard !== 'spade') {
+            currentCard = cards[Math.floor(Math.random() * 4)];
+            console.log(currentCard);
+        }
+    }
+
+function do_while() 
+    {
+        let cupsOfSugarNeeded = 2;
+        let cupsAdded = 0;
+
+        do {cupsAdded++; console.log(cupsAdded + ' cup was added') } 
+        while (cupsAdded < cupsOfSugarNeeded);
+    }
+
+function breakKey() 
+    {
+        const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+        for (let i = 0; i < rapperArray.length; i++){
+            console.log(rapperArray[i]);
+            if (rapperArray[i] === 'Notorious B.I.G.'){break;}
+        }
+        console.log("And if you don't know, now you know.");
+    };
